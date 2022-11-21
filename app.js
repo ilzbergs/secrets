7a1af9f2a32e7d2a5b736df164347ab4cc89c25b
Toms Ilzbergs <ilzbergs@inbox.lv>
2022-11-21 13:29:19 +0200

    Add hidden files

---
+++ app.js
@@ -0,0 +1,97 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const database = module.exports = () => {
+    const connectionsParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    };
+    try {
+        console.log("Database connected succesfully");
+        mongoose.connect("mongodb+srv://Toms:secrets2022@cluster0.xby4tiq.mongodb.net/test");
+    }
+    catch (error) {
+        console.log(error);
+        console.log("Database connection failed");
+    }
+};
+database();
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = process.env.SECRET 
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+
+const User = new mongoose.model('User', userSchema);
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+app.get('/register', function (req, res) {
+    res.render('register');
+});
+
+app.post('/register', function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+});
+
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username }, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render('secrets');
+                }
+            }
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function (req, res) {
+    console.log('Server started successfully!');
+});
